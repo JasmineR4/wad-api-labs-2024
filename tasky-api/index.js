192190c7ae916d61f9382cc59b09380b1080acc2
Jasmine <20098589@wit.ie>
2024-05-23 19:30:16 +0100

    Routes for get task and task details

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,5 +1,7 @@
+
 import dotenv from 'dotenv';
 import express from 'express';
+import tasksRouter from './api/tasks';
 
 dotenv.config();
 
@@ -7,7 +9,7 @@ const app = express();
 
 const port = process.env.PORT;
 
-app.use(express.static('public'));
+app.use('/api/tasks', tasksRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
